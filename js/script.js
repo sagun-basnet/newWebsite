dcf17344e413151cec9c6897cabcd7c897718537
sagun-basnet <78462500+sagun-basnet@users.noreply.github.com>
2022-01-25 20:39:50 +0545

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 // typing animation script
 var typing = new Typed(".typing", {
-    strings: ["Student", "Web Developer"],
+    strings: ["Student.", "Web Developer."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
-
