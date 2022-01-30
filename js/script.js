a6fb33db1af769b801b3466f32d6a0b49cdab367
sagun basnet <basnetsagun8@gmail.com>
2022-01-30 18:31:48 +0545

    new website 2 relese

--- js/script.js
+++ js/script.js
@@ -9,10 +9,13 @@ var typing = new Typed(".typing", {
 =======
 // typing animation script
 var typing = new Typed(".typing", {
-    strings: ["Student", "Web Developer"],
+    strings: ["Student.", "Web Developer."],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
+<<<<<<< HEAD
 
 >>>>>>> 0fa8d81 (all file added)
+=======
+>>>>>>> ebaf135 (new website 2 relese)
