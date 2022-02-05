1dbf633255fd0f93c898f047d6cea6bf26ebcef1
sagun basnet <basnetsagun8@gmail.com>
2022-02-05 18:09:20 +0545

    recover

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // typing animation script
 var typing = new Typed(".typing", {
     strings: ["Student.", "Web Developer."],
@@ -6,16 +5,4 @@ var typing = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 });
-=======
-// typing animation script
-var typing = new Typed(".typing", {
-    strings: ["Student.", "Web Developer."],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
-<<<<<<< HEAD
 
->>>>>>> 0fa8d81 (all file added)
-=======
->>>>>>> ebaf135 (new website 2 relese)
