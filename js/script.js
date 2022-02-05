014658b8140934accb1471a5b8883e897ccb4434
sagun basnet <basnetsagun8@gmail.com>
2022-02-05 17:22:52 +0545

    nothing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // typing animation script
 var typing = new Typed(".typing", {
     strings: ["Student.", "Web Developer."],
@@ -5,3 +6,13 @@ var typing = new Typed(".typing", {
     backSpeed: 60,
     loop: true
 });
+=======
+// typing animation script
+var typing = new Typed(".typing", {
+    strings: ["Student", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+>>>>>>> 0fa8d81 (all file added)
